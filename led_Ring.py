4f6cbaaab4d8d4825e7a69fe6a0468d2c764f90d
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-12-03 19:26:28 +0100

    do all

--- led_Ring.py
+++ led_Ring.py
@@ -2,32 +2,19 @@ import board
 import neopixel
 import time
 
-# Configuration
-pixel_pin = board.D18  # This is GPIO 18
-num_pixels = 5       # Change this to the number of LEDs on your ring
-ORDER = neopixel.GRB   # Most rings use Green-Red-Blue ordering
+# # Configuration
+# pixel_pin = board.D18  # This is GPIO 18
+# num_pixels = 5       # Change this to the number of LEDs on your ring
+# ORDER = neopixel.GRB   # Most rings use Green-Red-Blue ordering
 
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
-)
+# pixels = neopixel.NeoPixel(
+#     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+# )
 
-while True:
-    # Make them Red
-    pixels.fill((255, 0, 0))
+pixels = neopixel.NeoPixel(board.D18, 60, auto_write=False)
+
+for i in range(60):
+    pixels.fill((0, 0, 0))
+    pixels[i] = (255, 0, 0)
     pixels.show()
-    time.sleep(1)
-
-    # Make them Green
-    pixels.fill((0, 255, 0))
-    pixels.show()
-    time.sleep(1)
-
-    # Make them Blue
-    pixels.fill((0, 0, 255))
-    pixels.show()
-    time.sleep(1)
-
-    # Make them White
-    pixels.fill((255, 255, 255))
-    pixels.show()
-    time.sleep(1)
+    time.sleep(0.1)
