b8a0e2d2e81195e2474e28e6bd3e555ceb3734fb
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-12-07 17:06:37 +0100

    servo file

---
+++ servo.py
@@ -0,0 +1,34 @@
+import time
+from adafruit_servokit import ServoKit
+
+# Initialize the PCA9685 using the default address (0x40).
+# 'channels=16' tells the library we are using the 16-channel board.
+kit = ServoKit(channels=16)
+
+print("Moving Servo on Channel 0...")
+
+# The TD-8120MG usually has a range of 180 degrees.
+# Sometimes you need to adjust the pulse width range for specific servos,
+# but the defaults (1000-2000us) usually work for a basic test.
+
+try:
+    while True:
+        # Move to 0 degrees
+        print("0 degrees")
+        kit.servo[0].angle = 0
+        time.sleep(1)
+        
+        # Move to 90 degrees
+        print("90 degrees")
+        kit.servo[0].angle = 90
+        time.sleep(1)
+
+        # Move to 180 degrees
+        print("180 degrees")
+        kit.servo[0].angle = 180
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    # Turn off the servo signal on exit to stop jitter
+    kit.servo[0].angle = None
+    print("\nExiting")
