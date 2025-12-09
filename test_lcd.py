2db16d031812513cb208f135f4665d4fe3179f0e
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-12-09 18:59:05 +0100

    test

--- test_lcd.py
+++ test_lcd.py
@@ -9,37 +9,12 @@ def main():
         lcd = RGB1602(16, 2)
 
         # Test Colors
+        lcd.setRGB(255, 255, 255)
         lcd.setCursor(0, 0)
-        lcd.print("Hello! Start by")
+        lcd.print("Hello! Press to")
         lcd.setCursor(0, 1)
-        lcd.print("pressing the button")
-        time.sleep(3)
-        
-        lcd.setRGB(255, 0, 0)
-        time.sleep(1)
-        
-        lcd.setRGB(0, 255, 0)
-        lcd.setCursor(0, 1) # Move to start
-        lcd.print("Color: GREEN") 
-        time.sleep(1)
-        
-        lcd.setRGB(127, 0, 255)
-        lcd.setCursor(0, 0)
-        lcd.print("Color: VIOLET ")
-        time.sleep(1)
-        
-        # Test Text
-        print("Testing Text...")
-        lcd.setRGB(255, 255, 0) # White
-        lcd.clear()
-        
-        lcd.setCursor(0, 0)
-        lcd.print("Hello Nevo!")
-        
-        lcd.setCursor(0, 1) # Second line
-        lcd.print("It Works! :)")
-        
-        time.sleep(3)
+        lcd.print("start -->")
+        time.sleep(6)
         
         lcd.clear()
         lcd.print("Done.")
