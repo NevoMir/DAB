f81e10b185ce938a05309ceb79b405c0b1654113
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-12-09 23:46:09 +0100

    button

---
+++ button.py
@@ -0,0 +1,7 @@
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+while True:
+    if GPIO.input(17) == GPIO.HIGH:
+        print("Button Pressed")
