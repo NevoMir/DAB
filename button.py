ae82f679cfa3b5dfbca1dcbaf429f254eb45e686
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-12-09 23:47:06 +0100

    button change

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
-    if GPIO.input(17) == GPIO.HIGH:
+    if GPIO.input(4) == GPIO.HIGH:
         print("Button Pressed")
